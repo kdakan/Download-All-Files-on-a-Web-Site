2464d8806da0949f3a127e892e8d1c915efb2acc
Koray Dakan <kdakan@yahoo.com>
2019-03-22 18:01:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,17 +16,25 @@ namespace SiteDownloader
         static string baseUrl = @"https://doc.lagout.org/";
         static string startingPageUrl = @"https://doc.lagout.org/";
         static string baseDiskPath = @"e:\o";
-        
+        static long maxFileSize = 1024 * 1024 * 1024 / 2;//max half GB
+
         static StringBuilder logs = new StringBuilder();
+        static StringBuilder errorLogs = new StringBuilder();
+
         static HashSet<string> visitedFileUrls = new HashSet<string>();
         static HashSet<string> visitedPageUrls = new HashSet<string>();
-
+        
         static void Log(string line)
         {
             logs.AppendLine(line);
             Console.WriteLine(line);
         }
 
+        static void LogError(string line)
+        {
+            errorLogs.AppendLine(line);
+            Console.WriteLine(line);
+        }
         static void Main(string[] args)
         {
             ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
@@ -34,8 +42,9 @@ namespace SiteDownloader
 
             Log("Begin downloading site. Starting at: " + startingPageUrl);
             DownloadFilesOnPage(startingPageUrl);
-            Log("Site download complete.");
+            Log("Site download complete. Started at: " + startingPageUrl);
             File.WriteAllText(baseDiskPath + @"\download logs.txt", logs.ToString());
+            File.WriteAllText(baseDiskPath + @"\download error logs.txt", errorLogs.ToString());
             Console.ReadKey();
         }
 
@@ -48,6 +57,7 @@ namespace SiteDownloader
 
             if (ShouldIgnoreUrl(pageUrl))
                 return;
+
             var pageUrlDecoded = DecodeUrlString(pageUrl);
             Log("Begin downloading files on page: " + pageUrlDecoded);
 
@@ -56,30 +66,45 @@ namespace SiteDownloader
             var fileUrls = new HashSet<string>();
             var pageUrls = new HashSet<string>();
 
-            foreach (HtmlNode a in doc.DocumentNode.SelectNodes("//a[@href]"))
+            Parallel.ForEach(doc.DocumentNode.SelectNodes("//a[@href]"), new ParallelOptions { MaxDegreeOfParallelism = 8 }, a =>
             {
                 var href = a.GetAttributeValue("href", string.Empty);
-                if (string.IsNullOrEmpty(href) || href == @"../" || href.StartsWith("mailto:"))
-                    continue;
+                if (string.IsNullOrEmpty(href) ||
+                    href == @"../" ||
+                    href.StartsWith("mailto:") ||
+                    href.StartsWith("javascript:") ||
+                    href.StartsWith("#") ||
+                    href.StartsWith(@"ftp://")
+                    )
+                    return;
 
                 var fullUrl = getFullUrl(pageUrl, href);
                 if (!fullUrl.StartsWith(baseUrl))
-                    continue;
+                    return;
 
-                var contentType = GetContentType(fullUrl);
+                var fullPath = GetFullPath(fullUrl);
+                fullPath = FixInvalidLengthAndCharactersInFullPath(fullPath);
+                if (ShouldIgnoreUrl(fullUrl) || File.Exists(fullPath))
+                    return;
+
+                var contentTypeAndSize = GetContentTypeAndSize(fullUrl);
+                var contentType = contentTypeAndSize.Item1;
+                var size = contentTypeAndSize.Item2;
                 if (contentType.ToLowerInvariant().Contains("html"))
                     pageUrls.Add(fullUrl);
-                else
+                else if (size < maxFileSize)
                     fileUrls.Add(fullUrl);
-            }
+            });
 
-            foreach(var file in fileUrls)
-                DownloadFile(file);
+            Parallel.ForEach(fileUrls, new ParallelOptions { MaxDegreeOfParallelism = 8 }, url =>
+            {
+                DownloadFile(url);
+            });
 
             Log("Finished downloading files on page: " + pageUrlDecoded);
 
-            foreach (var page in pageUrls)
-                DownloadFilesOnPage(page);
+            foreach (var url in pageUrls)
+                DownloadFilesOnPage(url);
         }
 
         static string getFullUrl(string pageUrl, string href)
@@ -91,18 +116,15 @@ namespace SiteDownloader
             return pageUrl.TrimEnd('/') + @"/" + href.TrimStart('/');
         }
 
-        static void DownloadFile(string fullUrl)
+        static string GetFullPath(string fullUrl)
         {
-            if (visitedFileUrls.Contains(fullUrl))
-                return;
-
-            visitedFileUrls.Add(fullUrl);
-
             var fullPath = baseDiskPath + @"\" + fullUrl.Replace(baseUrl, "");
             fullPath = DecodeUrlString(fullPath).Replace(@"/", @"\");
-            if (ShouldIgnoreUrl(fullPath))
-                return;
+            return fullPath;
+        }
 
+        static string FixInvalidLengthAndCharactersInFullPath(string fullPath)
+        {
             var fileName = Path.GetFileName(fullPath);
             fileName = fileName.Replace(":", "");
             fileName = fileName.Replace("/w", "with");
@@ -115,6 +137,22 @@ namespace SiteDownloader
             var fileExtension = Path.GetExtension(fullPath);
             fullPath = fullPath.Substring(0, Math.Min(250, fullPath.Length - fileExtension.Length)) + fileExtension;//250 max path length
 
+            return fullPath;
+        }
+
+        static void DownloadFile(string fullUrl)
+        {
+            if (visitedFileUrls.Contains(fullUrl))
+                return;
+
+            visitedFileUrls.Add(fullUrl);
+
+            var fullPath = GetFullPath(fullUrl);
+            if (ShouldIgnoreUrl(fullPath))
+                return;
+
+            fullPath = FixInvalidLengthAndCharactersInFullPath(fullPath);
+
             Log(fullPath);
             var folderPath = Path.GetDirectoryName(fullPath);
             if (folderPath == fullPath.TrimEnd('\\'))
@@ -124,20 +162,29 @@ namespace SiteDownloader
                 Directory.CreateDirectory(folderPath);
             else if (File.Exists(fullPath))
                 return;
-
-            using (WebClient webClient = new WebClient())
+            try
             {
-                webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                webClient.DownloadFile(fullUrl, fullPath);
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.DownloadFile(fullUrl, fullPath);
+                }
             }
+            catch(Exception e)
+            {
+                LogError(fullPath);
+                LogError(e.Message);
+            }
+
         }
 
         static bool ShouldIgnoreUrl(string url)
         {
-            //Do not download long binaries..
             var urlLower = url.ToLowerInvariant();
             if (
                 urlLower.EndsWith(@"/distribution/") ||
+                urlLower.EndsWith(@"/network/") ||
+                urlLower.EndsWith(@"/s8zone_fichiers/") ||
                 urlLower.EndsWith(@"/mame/") ||
                 urlLower.EndsWith(@"/Belgian Hackers Zone/") || 
                 urlLower.EndsWith(@".iso") ||
@@ -170,9 +217,10 @@ namespace SiteDownloader
             return true;
         }
 
-        static string GetContentType(string url)
+        static Tuple<string, long> GetContentTypeAndSize(string url)
         {
             var contentType = @"text/html";
+            long size = 0;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             try
@@ -181,12 +229,15 @@ namespace SiteDownloader
                 using (Stream rstream = res.GetResponseStream())
                 {
                     contentType = res.Headers["Content-Type"];
+                    size = Convert.ToInt64(res.Headers["Content-Length"]);
                 }
                 res.Close();
             }
-            catch { }
+            catch (Exception ex)
+            {
+            }
 
-            return contentType;
+            return new Tuple<string, long>(contentType, size);
         }
         
     }
